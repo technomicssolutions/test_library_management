ba219281aa212f953b60b685f0064a27e41ca173
Rincy Titus <rincytitus5@gmail.com>
2014-10-13 11:56:17 +0530

    Changes in admin page

--- library_management_system/web/admin.py
+++ library_management_system/web/admin.py
@@ -1,10 +1,8 @@
 from django.contrib  import admin
 
-from library_management_app.models  import Student, BookCategory, Book, Issue
+from web.models  import Student, BookCategory, Book, Issue
 
 admin.site.register(Student)
 admin.site.register(BookCategory)
 admin.site.register(Book)
 admin.site.register(Issue)
-
-
